78d1dde4f5c08f9a63bfff9ac8116a9d839f8fd8
Kevin Finley <kftwotwo@gmail.com>
2016-12-16 11:57:08 -0800

    able to assign a shoe to a store

--- spec/intergeration_spec.rb
+++ spec/intergeration_spec.rb
@@ -22,40 +22,17 @@ describe('view all stylists or one', {:type => :feature}) do
     visit('/stores')
     expect(page).to have_content('Nike Outlet')
   end
-end
 
-# describe('add a client to a stylist', {:type => :feature}) do
-#   it('allows a user to add a client to a stylist') do
-#     @stylist.save()
-#     visit("/stylists/#{@stylist.id()}")
-#     fill_in("first_name", {:with => "John"})
-#     fill_in("last_name", {:with => "twotwo"})
-#     click_button('Add Client')
-#     expect(page).to have_content("John twotwo")
-#   end
-# end
-#
-# describe('add a stylists', {:type => :feature}) do
-#   it('allows the user to add a stylist in a list') do
-#     visit('/stylists')
-#     fill_in('first_name', :with => 'Table')
-#     fill_in('last_name', :with => 'Chair')
-#     click_button('Add Stylist')
-#     expect(page).to have_content('Table Chair')
-#   end
-# end
-#
-# describe('update one stylist', {:type => :feature}) do
-#   it('allows the user to update the name of a stylist or client') do
-#     visit('/stylists')
-#     fill_in('first_name', :with => 'sarina')
-#     fill_in('last_name', :with => 'nus')
-#     click_button('Add Stylist')
-#     click_link('sarina nus')
-#     click_link('Edit sarina nus')
-#     fill_in('new_first_name', :with => 'Jerry')
-#     fill_in('new_last_name', :with => 'Tom')
-#     click_button('Update')
-#     expect(page).to have_content('Jerry Tom')
-#   end
-# end
+  it "able to assign a shoe to a store" do
+    @store.save
+    @store2.save
+    visit('/shoes/new')
+    fill_in('new-name', :with => 'Air Flow 2.0')
+    fill_in('new-brand', :with => 'Nike')
+    fill_in('new-year', :with => '2001')
+    fill_in('new-kind', :with => 'Normal')
+    select("Nike Outlet", :from => 'store_id')
+    click_button('Enter')
+    expect(page).to have_content('Air Flow 2.0')
+  end
+end
